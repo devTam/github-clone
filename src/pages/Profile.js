71856e46f0285dc2a58d54e5fae2431675cce453
Teebreezy101 <tammy.batubo@gmail.com>
2020-12-05 03:29:36 +0100

    add tabbed nav

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/header/Header';
 import Sidebar from '../components/sidebar/Sidebar';
+import TabbedNav from '../components/tabbedNav/TabbedNav';
 import './profile.css';
 
 const Profile = () => {
+    const [ selectedTab, setSelectedTab] = useState('overview')
     
     return (
         <>
             <Header />
             <main className="main" >
                 <Sidebar />
+                <div className="main-repo">
+                    <TabbedNav selectedTab={selectedTab} setSelectedTab={setSelectedTab}/>
+                </div>
             </main>  
         </>
     )
